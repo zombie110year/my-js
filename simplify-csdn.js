b2b44d2708f847b68668ea5fe96275b66a24d647
zombie110year <zombie110year@outlook.com>
2019-06-29 15:52:21 +0800

    :recycle: 简化 CSDN 博客页面

--- simplify-csdn.js
+++ simplify-csdn.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         简化 CSDN
 // @namespace    http://js.zombie110year.top/
-// @version      0.1
-// @description  只保留文章和评论
+// @version      1.0
+// @description  简化 CSDN 博客页面
 // @license      GPL v3
 // @author       zombie110year@outlook.com
 // @match        *://blog.csdn.net/*/article/details/*
@@ -10,25 +10,20 @@
 // @run-at       document-end
 // ==/UserScript==
 
-(() => {
+/**
+ * MathJax 可正常运行
+ */
+(function () {
     'use strict';
-    let items = [];
-    items.push(document.querySelector("#csdn-toolbar"));
-    items.push(document.querySelector("#mainBox > aside"));
-    items.push(document.querySelector("#mainBox > main > div.recommend-box"));
-    items.push(document.querySelector("body > div.csdn-side-toolbar"));
-    items.push(document.querySelector("#mainBox > div"));
-    items.push(document.querySelector("body > div.tool-box"));
-
-    for(let _item of items) {
-        _item.parentElement.removeChild(_item);
-    }
+    //自动展开
+    document.querySelector(".btn-readmore").click();
+    //去除剪贴板劫持
+    csdn.copyright.init("", "", "");
+    // 获取博文主体
+    let _main = document.querySelector("#mainBox > main");
+    document.body = document.createElement("body");
+    document.body.append(_main);
 
     // 重设 样式
-    let _main = document.querySelector("#mainBox > main");
-    _main.style = "float: left !important; width: auto !important;";
-
-    // 自动展开文档
-    let btn = document.querySelector("#btn-readmore");
-    btn.click();
+    _main.style = "margin: 0px auto 40px auto !important; width: auto !important;";
 })();
